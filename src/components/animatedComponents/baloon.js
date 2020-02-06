519304d79bc91795f2b796eed79ae448ac4f1549
Aiden Barrett <aiden.e.barrett@gmail.com>
2020-02-06 12:47:25 +0000

    mobile balloon animation done

--- src/components/animatedComponents/baloon.js
+++ src/components/animatedComponents/baloon.js
@@ -13,16 +13,37 @@ const BaloonAnimation = () => {
     rootMargin: "60px",
   })
 
+  const cloud1Style = {
+    animation: inView ? "cloud 10s infinite linear" : "",
+  }
+
+  const cloud2Style = {
+    animation: inView ? "cloud 8s infinite linear 1s" : "",
+  }
+
+  const cloud3Style = {
+    animation: inView ? "cloud 7s infinite linear " : "",
+  }
+
+  const platformStyle = {
+    animation: inView ? "swing 1.2s infinite alternate ease-in-out" : "",
+  }
+
   return (
     <div ref={ref} className="balloon-container">
       <div className="balloon-wrapper">
         <img src={balloon} alt="Air baloon" className="balloon" />
-        <img src={platform} alt="Air baloon platform" className="platform" />
+        <img
+          style={platformStyle}
+          src={platform}
+          alt="Air baloon platform"
+          className="platform"
+        />
       </div>
       <div className="cloud-wrapper">
-        <img src={cloud1} alt="cloud" className="cloud1" />
-        <img src={cloud2} alt="cloud" className="cloud2" />
-        <img src={cloud3} alt="cloud" className="cloud3" />
+        <img style={cloud1Style} src={cloud1} alt="cloud" className="cloud1" />
+        <img style={cloud2Style} src={cloud2} alt="cloud" className="cloud2" />
+        <img style={cloud3Style} src={cloud3} alt="cloud" className="cloud3" />
       </div>
     </div>
   )
