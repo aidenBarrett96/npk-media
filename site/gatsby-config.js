394db6215822dc3a7abd78d4deeeb25b4b403167
Aiden Barrett <aiden.e.barrett@gmail.com>
2020-07-08 15:51:32 +0100

    linking gatsby config to ngrok strapi server

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
 			options: {
 				typeName: 'Strapi',
 				fieldName: 'strapi',
-				url: 'http://localhost:1337/graphql',
+				url: 'https://850d6d5e0f43.ngrok.io/graphql',
 			},
 		},
 		/* {
