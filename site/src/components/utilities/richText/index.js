66c6d7f0c4444d597ebd8780191132d3a06b68df
Aiden Barrett <aiden.e.barrett@gmail.com>
2020-09-15 15:44:29 +0100

    added codeblok to rich text resolver

--- site/src/components/utilities/richText/index.js
+++ site/src/components/utilities/richText/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { render } from 'storyblok-rich-text-react-renderer';
 const RichText = ({ blok }) => {
 	const { body } = blok;
-	return <div>{render(body)}</div>;
+	return (
+		<div>
+			{render(body, {
+				nodeResolvers: {
+					[NODE_CODEBLOCK]: (children) => (
+						<>
+							<h1>CODEBLOK</h1>
+							<div dangerouslySetInnerHTML={children} />
+						</>
+					),
+				},
+			})}
+		</div>
+	);
 };
 
 export default RichText;
