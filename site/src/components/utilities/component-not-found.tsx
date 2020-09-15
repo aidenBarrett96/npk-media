220150eda25a0aaeea841dc858f1a4ec1e4d4929
Aiden Barrett <aiden.e.barrett@gmail.com>
2020-09-15 15:33:44 +0100

    styled component not found

--- site/src/components/utilities/component-not-found.tsx
+++ site/src/components/utilities/component-not-found.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { IStoryblokComponent } from '../../types/storyblok';
 
 const ComponentNotFound: React.FC<IStoryblokComponent> = ({ blok }) => (
-	<div>Component {blok.component} is not defined. Add it to components.js</div>
+	<div style={{ border: '1px solid black', padding: '24px', margin: '40px 0' }}>
+		Component {blok.component} is not defined. Add it to components.js
+	</div>
 );
 
 export default ComponentNotFound;
