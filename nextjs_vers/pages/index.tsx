67648676a30444d297813f3671d79c9a370de31e
Gareth May <garethmay@hotmail.co.uk>
2021-01-07 11:51:42 +0000

    homepage data

--- nextjs_vers/pages/index.tsx
+++ nextjs_vers/pages/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react"
 import Layout from "../components/layout/Layout"
 import StoryblokService from '../utils/storyblok-service'
 
-const HomePage = () => {
+const HomePage = ({res}) => {
+
+  const [content, setContent] = useState([])
+
+  useEffect(() => {
+    async function getContent() {
+      const res = await StoryblokService.get('cdn/stories/home', {})
+      setContent([res])
+    }
+    getContent()
+  }, [])
+
   return (
     <Layout>
       <div> 
         Homepage
+        <br/>
+        {console.log(content)}
       </div>
     </Layout>
   )
@@ -14,12 +28,17 @@ export default HomePage
 
 
 // Query to get data from cms
-export async function getInitialProps({ query }) {
-  StoryblokService.setQuery(query)
+// export async function getInitialProps({ query }) {
 
-  let res = await StoryblokService.get('cdn/stories/home', {})
+//   StoryblokService.setQuery(query)
 
-  return {
-    res
-  }
-}
+//   let res = await StoryblokService.get('cdn/stories/home', {})
+
+//   return {
+//     res
+//   }
+
+  
+// }
+
+
