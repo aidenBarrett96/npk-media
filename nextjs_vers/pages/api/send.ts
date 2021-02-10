38bcffc92467e4827cf28082609f3d1bdf3778a9
Gareth May <garethmay@hotmail.co.uk>
2021-02-10 11:27:09 +0000

    correct send-to email in send.ts

--- nextjs_vers/pages/api/send.ts
+++ nextjs_vers/pages/api/send.ts
@@ -1,15 +1,12 @@
 const sgMail = require('@sendgrid/mail')
 
-
-// 'to' email address should be the inbox that will receive all enquiries
-
 export default async function(req, res) {
   sgMail.setApiKey(process.env.NEXT_PUBLIC_SG_API_KEY) 
 
   const { name, email, message } = req.body
 
   const formContent = {
-    to: 'dev@npkmedia.co.uk',
+    to: 'web@npkmedia.co.uk',
     from: 'no-reply@npk.media',
     subject: `New Site Enquiry From - ${name}`,
     text: message,
